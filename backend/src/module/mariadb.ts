16c67b8d7600e4fb9dc7f9f2208d7dafc2833d0a
bagger3025 <80453100+bagger3025@users.noreply.github.com>
2022-11-17 11:12:23 +0000

    Mariadb 세팅 및 graphql 세팅

---
+++ backend/src/module/mariadb.ts
@@ -0,0 +1,11 @@
+import * as mariadb from 'mariadb';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWD,
+  database: process.env.DB_DBNAME,
+  connectionLimit: 5,
+});
